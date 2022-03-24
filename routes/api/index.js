9550a22b7b1cb2adc90b409ae525f54fb7be19a6
Spencer Kerans <skerans0@gmail.com>
2022-03-23 21:20:08 -0600

    some location routes set up

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
+const locationRoutes = require('./locationRoutes');
+const travelerRoutes = require('./travelerRoutes');
 
-
+router.use('/locations', locationRoutes);
+router.use('/travelers', travelerRoutes);
 
 module.exports = router;
---
+++ routes/api/locationRoutes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+const { traveler, location, trips } = require('../../models');
+
+// GET all locations
+router.get('/', async (req, res) => {
+  try {
+    const locationData = await location.findAll();
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+// CREATE a location
+router.post('/', async (req, res) => {
+  try {
+    const locationData = await location.create({
+      location_id: req.body.location_id,
+    });
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+
+
+
+module.exports = router;
